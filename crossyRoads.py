74613686cbbce2b6cc8d15c187f8b81aa1ff571f
Lujine <lujine_2005@yahoo.com>
2020-02-05 19:13:00 -0500

    player_movement

--- crossyRoads.py
+++ crossyRoads.py
@@ -16,6 +16,12 @@ gridPixelSize = (spritePixelSize * tileScaling)
 playStartX = 500
 playStartY = 16
 
+playerMovementSpeed = 5
+
+up = arcade.key.UP
+left = arcade.key.LEFT
+right = arcade.key.RIGHT
+
 def loadTexture(filename):
     return arcade.load_texture(filename)
 
@@ -45,6 +51,23 @@ class MyGame(arcade.Window):
         self.playerSprite.center_x = playStartX
         self.playerSprite.center_y = playStartY
         self.playerList.append(self.playerSprite)
+
+
+    def on_key_press(self, key, modifiers):
+        if key == up:
+            self.player_sprite.change_y = playerMovementSpeed
+        elif key == left:
+            self.player_sprite.change_x = -playerMovementSpeed
+        elif key == right:
+            self.player_sprite.change_x = playerMovementSpeed
+
+    def on_key_release(self, key, modifiers):
+        if key == up:
+            self.player_sprite.change_y = 0
+        elif key == left:
+            self.player_sprite.change_x = 0
+        elif key == right:
+            self.player_sprite.change_x = 0
 
     def on_draw(self):
         arcade.start_render()
